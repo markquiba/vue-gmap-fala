d224b4d4f0ed69dfca38fdbefd8d91457fe77d27
markquiba <markjquiba@gmail.com>
2019-08-08 05:30:39 +0800

    Create Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="this.$store.state.loader">
+    <div class="spinner-grow spinner-grow-sm text-dark mx-2" />
+    <div class="spinner-grow spinner-grow-sm text-secondary mx-2" />
+    <div class="spinner-grow spinner-grow-sm text-info mx-2" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader'
+}
+</script>
