dad9f436f99c1ef92383cb8c5b70dff29fee85ff
markquiba <markjquiba@gmail.com>
2019-08-07 12:05:16 +0800

    Instantiate vue google maps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+const VueGoogleMaps = require('vue2-google-maps')
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_API_KEY,
+    libraries: 'places'
+  }
+})
 
 Vue.config.productionTip = false
 
