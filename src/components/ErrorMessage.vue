e45d1e803a7fb96e5d6b4daf7d927492cc6fafc7
markquiba <markjquiba@gmail.com>
2019-08-08 05:31:07 +0800

    Create ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="error_message">
+    <h6 class="text-danger">{{ error_message }}</h6>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'ErrorMessage',
+  computed: {
+    ...mapState(['error_message'])
+  }
+}
+</script>
