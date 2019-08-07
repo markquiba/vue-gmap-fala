bc6d7e9dadbcec65365c766684f1a570e4373f95
markquiba <markjquiba@gmail.com>
2019-08-08 05:32:26 +0800

    Create RouteInfo component

---
+++ src/components/RouteInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="total_distance && total_time">
+    <h6><strong>Total Distance: </strong> {{ total_distance }}</h6>
+    <h6><strong>Total Time: </strong> {{ total_time }}</h6>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'RouteInfo',
+  computed: {
+    ...mapState(['total_distance', 'total_time'])
+  }
+}
+</script>
