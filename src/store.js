6f490c658f87493938c048d3269b161fde476858
markquiba <markjquiba@gmail.com>
2019-08-08 05:34:05 +0800

    Manage state changes, mutations and actions

--- src/store.js
+++ src/store.js
@@ -3,40 +3,93 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const url = process.env.VUE_APP_BACKEND_URL
+
 export default new Vuex.Store({
   state: {
-    loader: false,
-    origin: 'origin',
-    destination: 'destination'
+    origin: '',
+    destination: '',
+    total_distance: null,
+    total_time: null,
+    path_coords: [],
+    error_message: null,
+    loader: false
+  },
+  getters: {
+    origin_coords (state) {
+      return state.path_coords[0]
+    },
+    destination_coords (state) {
+      return state.path_coords.slice(-1)[0]
+    }
   },
   mutations: {
     setLoader (state, payload) {
       state.loader = payload
+    },
+    clearInput (state, textInput) {
+      state[textInput] = ''
+    },
+    updateInput (state, { textInput, payload }) {
+      state[textInput] = payload
+    },
+    updateErrorMessage (state, payload) {
+      state.error_message = payload
+    },
+    updateInfo (state, { textInfo, payload }) {
+      state[`total_${textInfo}`] = payload
+    },
+    updatePathCoordinates (state, payload) {
+      state.path_coords = payload
     }
   },
   actions: {
-    fetchToken ({ commit }, params) {
+    FETCH_TOKEN ({ commit, dispatch }) {
       commit('setLoader', true)
-      return new Promise((resolve, reject) => {
-        fetch(`http://localhost:8080/route`, {
+      return new Promise((resolve) => {
+        fetch(`${url}/route`, {
           method: 'POST',
           body: JSON.stringify({
             origin: this.state.origin,
             destination: this.state.destination
           })
-        }).then(res => res.json()
-        ).then(data => resolve(data)
-        ).catch(error => console.log(error))
+        }).then(response => {
+          if (!response.ok) {
+            dispatch('FETCH_TOKEN')
+            throw new Error(`HTTP Error: ${response.status}`)
+          }
+          return response.json()
+        }).then(data => {
+          dispatch('FETCH_ROUTE', data.token)
+        })
       })
     },
-    fetchRoute ({ commit }, { token }) {
+    FETCH_ROUTE ({ commit, dispatch }, token) {
       return new Promise((resolve, reject) => {
-        fetch(`http://localhost:8080/route/${token}`
-        ).then(res => res.json()
-        ).then(data => {
-          resolve(data)
-          commit('setLoader', false)
-        }).catch(error => console.log(error))
+        fetch(`${url}/route/${token}`
+        ).then(response => {
+          if (!response.ok) {
+            dispatch('FETCH_ROUTE', token)
+            console.log('HTTPS status', response.status)
+          }
+          return response.json()
+        }).then(response => {
+          if (response.status === 'success') {
+            let paths = []
+            response.path.forEach(i => {
+              paths.push({ lat: parseFloat(i[0]), lng: parseFloat(i[1]) })
+            })
+            commit('updatePathCoordinates', paths)
+            commit('updateInfo', { textInfo: 'distance', payload: response.total_distance })
+            commit('updateInfo', { textInfo: 'time', payload: response.total_time })
+            commit('setLoader', false)
+          } else if (response.status === 'failure') {
+            commit('updateErrorMessage', response.error)
+            commit('setLoader', false)
+          } else {
+            dispatch('FETCH_ROUTE', token)
+          }
+        }).then(data => resolve(data))
       })
     }
   }
