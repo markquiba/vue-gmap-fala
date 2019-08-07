5238b414b2fe675fb6d5792a20ddd0d2ffc57343
markquiba <markjquiba@gmail.com>
2019-08-07 12:06:12 +0800

    Create GoogleMap component

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,26 @@
+<template>
+  <GmapMap
+    style="height: 100vh;"
+    ref="mapRef"
+    :center="{lat: 14.5995, lng: 120.9842}" :zoom="12"
+    :options="{
+      zoomControl: true,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: true,
+      disableDefaultUi: false
+    }"
+  >
+  </GmapMap>
+</template>
+
+<script>
+export default {
+  name: 'GoogleMap'
+}
+</script>
+
+<style scoped>
+
+</style>
